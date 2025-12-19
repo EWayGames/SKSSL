08c97a3397a5aaad525861ee31b8cb406f5546c1
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2025-12-19 04:00:25 -0600

    Added universally-useful GamePathing folder w. dynamic path handling.

--- GamePathing.cs
+++ GamePathing.cs
@@ -1,28 +1,57 @@
+using System.Diagnostics.CodeAnalysis;
+// ReSharper disable UnusedType.Global
+
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 namespace SKSSL;
 
 public static class GamePathing
 {
-    public static readonly string GAME_ENVIRONMENT = Path.Combine(Environment.CurrentDirectory);
-    
-    private static string FOLDER_GAME;
-    private static string FOLDER_MODS;
-    
+    private static readonly Dictionary<string, string> GAME_PATHS = new();
+    public static readonly string GAME_ENVIRONMENT = AppContext.BaseDirectory;
+    public static readonly string PROJECT_DIRECTORY = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "..", "..", ".."));
+    public static readonly string FOLDER_GAME = Path.Combine(GAME_ENVIRONMENT, "game");
+    public static readonly string FOLDER_MODS = Path.Combine(GAME_ENVIRONMENT, "mods");
+    public static readonly string FOLDER_LOCALE = Path.Combine(GAME_ENVIRONMENT, "localization");
+
     /// <returns>Dedicated path to game files.</returns>
-    public static string GPath(params string[] path) => Path.Combine(new[] { FOLDER_GAME }.Concat(path).ToArray());
+    public static string GPath(params string[] path)
+    {
+        var dynamicPath = GetPath("FOLDER_GAME");
+        return string.IsNullOrEmpty(dynamicPath)
+            ? Path.Combine(new[] { FOLDER_GAME }.Concat(path).ToArray())
+            : dynamicPath;
+    }
 
     /// <returns>A game-path explicitly for the "mods" folder.</returns>
     /// <seealso cref="GPath"/>
-    public static string MPath(params string[] path) => Path.Combine(new[] { FOLDER_MODS }.Concat(path).ToArray());
-    
-    /// <summary>
-    /// Initializes the game's 
-    /// </summary>
-    /// <param name="gameDirectory"></param>
-    /// <param name="modsDirectory"></param>
-    public static void Initialize(string gameDirectory, string modsDirectory)
+    public static string MPath(params string[] path)
     {
-        FOLDER_GAME = Path.Combine(GAME_ENVIRONMENT, gameDirectory);
-        FOLDER_MODS = Path.Combine(GAME_ENVIRONMENT, modsDirectory);
+        var dynamicPath = GetPath("FOLDER_MODS");
+        return string.IsNullOrEmpty(dynamicPath)
+            ? Path.Combine(new[] { FOLDER_MODS }.Concat(path).ToArray())
+            : dynamicPath;
+    }
+
+    public static string Proj(params string[] path)
+    {
+        var dynamicPath = GetPath("PROJECT_DIRECTORY");
+        return string.IsNullOrEmpty(dynamicPath)
+            ? Path.Combine(new[] { PROJECT_DIRECTORY }.Concat(path).ToArray())
+            : dynamicPath;
+    }
+    
+    private static string? GetPath(string id)
+    {
+        GAME_PATHS.TryGetValue(id, out var result);
+        return result;
+    }
+
+    /// <summary>
+    /// Initializes the game's two primary directories.
+    /// </summary>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public static void Initialize(params (string id, string path)[] paths)
+    {
+        foreach ((string id, string path) path in paths) GAME_PATHS[path.id] = path.path;
     }
 }
