bdd82bd64dd6b727b97bb72f94b08dcfb8e8adf8
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2025-12-22 20:22:41 -0600

    Merged GlobalHelpers Files Into Game Loader

--- GameLoader.cs
+++ GameLoader.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using Microsoft.Xna.Framework.Content;
 
 namespace SKSSL.Registry;
 public static class GameLoader
@@ -11,10 +12,33 @@ public static class GameLoader
     
     /// <summary>
     /// Default Localization path.
-    /// <example>GameName/game/localization/en-US/...</example>
+    /// <example>GameName/game/localization/...</example>
     /// </summary>
     public static string FOLDER_LOCALIZATION = Path.Combine(FOLDER_GAME, "localization");
-
+    
+    /// <summary>
+    /// Returns enumerated files from a specific folder path respectful to the program's executable.
+    /// Will attempt to use the Application Context's Base Directory as its root if a directory is not provided.
+    /// </summary>
+    public static IEnumerable<string> GetGameFiles(string? directory = null, params string[] path_s)
+    {
+        string fullPath = Path.Combine(directory ?? AppContext.BaseDirectory, Path.Combine(path_s));
+        return Directory.EnumerateFiles(fullPath, "*", SearchOption.AllDirectories);
+    }
+    
+    /// <summary>
+    /// Retrieves an asset from the content pipeline manually using a provided filepath.
+    /// </summary>
+    /// <param name="contentManager"></param>
+    /// <param name="path"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T GetPipelineAsset<T>(ContentManager contentManager, params string[] path)
+    {
+        string assetPath = Path.Combine(path);
+        return contentManager.Load<T>(assetPath);
+    }
+    
     /// <returns>Dedicated path to game files.</returns>
     public static string GPath(params string[] path)
     {
