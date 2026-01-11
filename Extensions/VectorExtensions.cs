fcf9a5ef135ea6faaea6254f6a87716c2d2403f6
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2026-01-11 02:11:43 -0600

    Added AsXnaColor() Function to VectorExtensions.cs

--- Extensions/VectorExtensions.cs
+++ Extensions/VectorExtensions.cs
@@ -5,4 +5,5 @@ namespace SKSSL.Extensions;
 public static class VectorExtensions
 {
     public static bool IsZero(this Vector4 vector) => vector.X == 0 && vector.Y == 0 && vector.Z == 0 && vector.W == 0;
+    public static Color AsXnaColor(this Vector4 vector) => new(vector.W, vector.X, vector.Y, vector.Z);
 }
