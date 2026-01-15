9795c5069077b810f02c3e17a8a99a2d3b8d3020
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2026-01-14 23:39:23 -0600

    Added ++ Operator to IDIterator.cs

--- Utilities/IDIterator.cs
+++ Utilities/IDIterator.cs
@@ -7,8 +7,14 @@ namespace SKSSL.Utilities;
 /// <param name="Maximum"></param>
 public class IDIterator(int InitialId = 0, int Maximum = -1)
 {
+    #region Operator Overloads
+
     public static implicit operator int(IDIterator iterator) => iterator.ID;
     public static implicit operator IDIterator(int id) => new(id);
+    public static IDIterator operator ++(IDIterator iterator) => iterator.Iterate();
+
+    #endregion
+
     public int ID { get; internal set; } = InitialId;
     public int Iterate()
     {
@@ -19,5 +25,4 @@ public class IDIterator(int InitialId = 0, int Maximum = -1)
     }
 
     public override string ToString() => ID.ToString();
-    
 }
