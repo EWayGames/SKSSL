e4dc38ae759220e016c8d4ffe0ce1a8ee63dca15
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2026-01-14 23:57:10 -0600

    Added RegistryProvider

---
+++ RegistryProvider.cs
@@ -0,0 +1,24 @@
+namespace KBSL.Definitions.Networking;
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class RegistryProvider<T> : IServiceProvider where T : class, new()
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public T? Registry { get; private set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Replace(T replacement) => Registry = replacement;
+
+    /// <inheritdoc />
+    public object? GetService(Type serviceType)
+    {
+        return serviceType == typeof(T) ? Registry : null;
+    }
+}
