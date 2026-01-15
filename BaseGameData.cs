d4ee9a04e1e29b8e921fa32734e18381f2ceee55
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2026-01-14 21:45:19 -0600

    Added some precautions to BaseGameData.cs

--- BaseGameData.cs
+++ BaseGameData.cs
@@ -1,5 +1,7 @@
 namespace SKSSL;
 
+using static DustLogger;
+
 public abstract class BaseGameData
 {
     /// <summary>
@@ -8,7 +10,13 @@ public abstract class BaseGameData
     /// <param name="paths"></param>
     public void Load(string[] paths)
     {
-        foreach (var path in paths) Load(path);
+        foreach (var path in paths)
+        {
+            if (!string.IsNullOrEmpty(path))
+                Load(path);
+            else
+                Log($"Missing filepath in LoadContent() call in {nameof(BaseGameData)}", LOG.SYSTEM_ERROR);
+        }
     }
 
     /// <summary>
