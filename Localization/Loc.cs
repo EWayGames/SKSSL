68ac372583462bb43f8ef2306c11ae4262c6a2f6
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2026-01-15 23:30:47 -0600

    Fixed NameSpace Using in Loc.cs

--- Localization/Loc.cs
+++ Localization/Loc.cs
@@ -1,7 +1,6 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Globalization;
-using SKSSL.Registry;
 // ReSharper disable UnusedType.Global
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
