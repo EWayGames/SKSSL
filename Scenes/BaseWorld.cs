ae7a136bf386d7765b3785d094da745bbdd22e90
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2026-01-15 20:29:10 -0600

    BaseWorld is no Longer Virtual, and now uses Worldspace

--- Scenes/BaseWorld.cs
+++ Scenes/BaseWorld.cs
@@ -1,5 +1,7 @@
 using Microsoft.Xna.Framework;
 using SKSSL.ECS;
+using SKSSL.Space;
+
 // ReSharper disable PublicConstructorInAbstractClass
 
 namespace SKSSL.Scenes;
@@ -7,10 +9,15 @@ namespace SKSSL.Scenes;
 public abstract class BaseWorld
 {
     public ECSController ECS { get; }
+    
+    public abstract IRenderableSpace WorldSpace { get; }
 
     public virtual bool HasECS => false;    
     
-    public BaseWorld() => ECS = new ECSController(this);
+    public BaseWorld()
+    {
+        ECS = new ECSController(this);
+    }
 
     /// <summary>
     /// Called by <see cref="BaseScene"/> initialization.
@@ -21,22 +28,25 @@ public abstract class BaseWorld
         // If programmer forces ECS to be enabled, toggle it in initialize.
         if (HasECS)
             ECS.Initialize();
+        WorldSpace.Initialize();
     }
 
-    public virtual void Update(GameTime gameTime)
+    public void Update(GameTime gameTime)
     {
         ECS.Update(gameTime);
+        WorldSpace.Update();
     }
 
-    public virtual void Draw(GameTime gameTime)
+    public void Draw(GameTime gameTime)
     {
         ECS.Draw(gameTime);
+        WorldSpace.Draw();
     }
 
     /// <summary>
     /// Ensures that this world instance is safely deleted before being replaced.
     /// </summary>
-    public virtual void Destroy()
+    public void Destroy()
     {
         ECS.Destroy();
     }
