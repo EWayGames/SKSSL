761fad9ba23752b6d9dfc50dae1569270568a70b
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2026-01-21 15:02:07 -0600

    Added "Fields" Region to EntityTemplate.cs

--- ECS/EntityTemplate.cs
+++ ECS/EntityTemplate.cs
@@ -9,22 +9,27 @@ namespace SKSSL.YAML;
 /// Best used for systems where entities are complex and require templating.
 public record EntityTemplate : AEntityCommon
 {
+    #region Fields
+
     /// <inheritdoc/>
     internal override string Handle { get; init; }
 
     /// <inheritdoc/>
     internal override string NameKey { get; set; }
-    
+
     /// <inheritdoc/>
     internal override string DescriptionKey { get; set; }
-    
+
     /// <inheritdoc/>
     public override IReadOnlyDictionary<Type, object> DefaultComponents { get; init; }
-    
+
     /// <summary>
     /// Can be overwritten to allow for safe type-casting.
     /// </summary>
     public virtual Type EntityType => typeof(SKEntity);
+
+    #endregion
+
     protected EntityTemplate(EntityYaml yaml, IReadOnlyDictionary<Type, object> components) : base(yaml, components)
     {
     }
@@ -41,7 +46,6 @@ public record EntityTemplate : AEntityCommon
         Dictionary<Type, object> components)
         where TTemplate : EntityTemplate
     {
-
         if (Activator.CreateInstance(
                 typeof(TTemplate),
                 BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic,
