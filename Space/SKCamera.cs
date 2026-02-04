45289c513852cee87e507fc24096e4b31cfee7b5
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2026-02-04 15:01:51 -0600

    Updated SKCamera.cs

--- Space/SKCamera.cs
+++ Space/SKCamera.cs
@@ -1,38 +1,50 @@
 using Microsoft.Xna.Framework;
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 
 namespace SKSSL.Space;
 
+/// Three-Dimensional Camera For Use in a 3D Space
 public class SK3DCamera
 {
     /// Camera's target. (The thing it's pointed at.)
-    private Vector3 target;
+    private Vector3 Target;
 
     /// Camera's position.
-    private Vector3 position;
+    public Vector3 Position { get; private set; }
 
     /// Camera's projection matrix.
-    private Matrix projection;
+    public Matrix Projection { get; private set; }
 
     /// Camera's view matrix.
-    private Matrix view;
+    public Matrix View { get; private set; }
 
     /// World matrix.
-    private Matrix world;
+    public Matrix World { get; private set; }
 
     /// Is this camera focused around a single point?
-    private bool orbit = false;
+    public bool IsOrbit { get; private set; } = false;
 
+    /// Resets camera Target and Position to Zero.
     public void ResetPosition()
     {
-        target = Vector3.Zero;
-        position = Vector3.Zero;
+        Target = Vector3.Zero;
+        Position = Vector3.Zero;
     }
 
+    /// Resets Camera to Default Values
     public void Default(GraphicsDeviceManager graphics)
     {
-        projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45f),
+        Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45f),
             graphics.GraphicsDevice.Viewport.AspectRatio, 1f, 1000f);
-        view = Matrix.CreateLookAt(position, target, new Vector3(0f, 1f, 0f)); // Y up
-        world = Matrix.CreateWorld(target, Vector3.Forward, Vector3.Up);
+        View = Matrix.CreateLookAt(Position, Target, new Vector3(0f, 1f, 0f)); // Y up
+        World = Matrix.CreateWorld(Target, Vector3.Forward, Vector3.Up);
+    }
+
+    /// Assigns Projection, View, and World Matrices to Camera
+    public void Set(Matrix projection, Matrix view, Matrix world)
+    {
+        Projection = projection;
+        View = view;
+        World = world;
     }
 }
