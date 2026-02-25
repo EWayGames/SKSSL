c3b35aa850d70995b6b3471d09b53e6047851a12
latenightbuddha <latenightbuddha@users.noreply.github.com>
2026-02-25 16:37:30 -0500

    This code is trash, I will need to rework this at a later date.

--- Networking/UserClient.cs
+++ Networking/UserClient.cs
@@ -1,13 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: SKSSL.Networking.UserClient
-// Assembly: SKSSL, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 8B98CDD1-E58F-4F79-8D54-EFB305228BC6
-// Assembly location: D:\DEVBOOK_6\DECOMP\CS\SKSSL.dll
-
-using System.Net;
-using System.Net.Sockets;
-
-namespace SKSSL.Networking;
+﻿namespace SKSSL.Networking;
 
 public class UserClient // IMPL: No networking has been achieved as of 20251215. One day...!
 {
@@ -22,20 +13,5 @@ public class UserClient // IMPL: No networking has been achieved as of 20251215.
     public void Update()
     {
         
-    }
-
-    public Socket Socket { get; set; }
-
-    public byte[] Buffer { get; set; }
-
-    public UserClient(Socket socket)
-    {
-        this.Socket = socket;
-        this.Buffer = new byte[1024];
-    }
-
-    public EndPoint RemoteEndPoint
-    {
-        get => this.Socket == null ? (EndPoint)null : this.Socket.RemoteEndPoint;
     }
 }
