cb46e9a57adef18d016fa55528b142e0419b1def
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2026-03-10 19:51:55 -0500

    Refactored SKMaterial.cs

--- Textures/SKMaterial.cs
+++ Textures/SKMaterial.cs
@@ -2,18 +2,44 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace SKSSL.Textures;
 
+// ReSharper disable UnusedMember.Global
 /// <summary>
 /// Texture mapping fields mapped to various types of textures.
 /// <example>Diffuse, Normal, Displacement, etc.</example>
 /// </summary>
-public struct SKMaterial
+public record SKMaterial
 {
+    /// Internal array of texture references used in this material.
+    public readonly Texture2D?[] Textures = new Texture2D?[4];
+    
     // @formatter:off
     /// Albedo / color map.
-    public Texture2D? Diffuse { get; set; }
-    public Texture2D? Normal { get; set; } // Normal map
-    public Texture2D? Displacement { get; set; } // Height map
-    public Texture2D? Emissive { get; set; }
+    public Texture2D? Diffuse
+    {
+        get => Textures[0];
+        set => Textures[0] = value;
+    }
+
+    /// Lighting height map.
+    public Texture2D? Normal
+    {
+        get => Textures[1];
+        set => Textures[1] = value;
+    }
+
+    /// Physical displacement map.
+    public Texture2D? Displacement
+    {
+        get => Textures[2];
+        set => Textures[2] = value;
+    }
+
+    /// Lighting emission map.
+    public Texture2D? Emissive
+    {
+        get => Textures[3];
+        set => Textures[3] = value;
+    }
     // IMPL: occlusion, detail mask, etc. Everything below is a bit out-of-current-scope.
     //public Texture2D? Specular { get; set; }
     //public Texture2D? Metallic { get; set; }
@@ -23,4 +49,26 @@ public struct SKMaterial
     // @formatter:on
 
     // Positional constructor for normal use
+
+    /// Emissive, Displacement, etc. are null. Diffuse is the only layer given a texture.
+    /// <returns>Default error texture when a material is not found.</returns>
+    public static SKMaterial Error(Texture2D errorTexture) => new() { Diffuse = errorTexture };
+
+    /// Blank constructor for creating materials incrementally.
+    public SKMaterial()
+    {
+    }
+
+    /// Constructor to manually assign all textures.
+    public SKMaterial(
+        Texture2D diffuse,
+        Texture2D normal,
+        Texture2D displacement,
+        Texture2D emissive)
+    {
+        Diffuse = diffuse;
+        Normal = normal;
+        Displacement = displacement;
+        Emissive = emissive;
+    }
 }
