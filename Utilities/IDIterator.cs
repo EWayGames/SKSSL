45547624612d23ad116ad1fd8b2f8888b14b8c0e
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2026-03-17 21:17:25 -0500

    Added comments to IDIterator.cs

--- Utilities/IDIterator.cs
+++ Utilities/IDIterator.cs
@@ -9,20 +9,29 @@ public class IDIterator(int InitialId = 0, int Maximum = -1)
 {
     #region Operator Overloads
 
+    /// Add two Iterator values together to assign new ID.
     public static implicit operator int(IDIterator iterator) => iterator.ID;
+
+    /// Add integer value to an Iterator to produce a new one.
     public static implicit operator IDIterator(int id) => new(id);
+
+    /// ++Operator against iterator dictates <see cref="Iterate"/> call.
     public static IDIterator operator ++(IDIterator iterator) => iterator.Iterate();
 
     #endregion
 
-    public int ID { get; internal set; } = InitialId;
+    /// Initialized integer ID value with initial ID from constructor.
+    public int ID { get; private set; } = InitialId;
+
+    /// Increments internal ID value by one, up to a maximum, if there is any defined.
     public int Iterate()
     {
         int nextId = ID++;
         if (Maximum != -1 && ID > Maximum)
-            throw new IndexOutOfRangeException("Too many voxel definitions for maximum voxel count!");
+            throw new IndexOutOfRangeException("Iterator exceeds maximum value on iterate call!");
         return nextId;
     }
 
+    /// <inheritdoc cref="System.Object.ToString"/>
     public override string ToString() => ID.ToString();
 }
