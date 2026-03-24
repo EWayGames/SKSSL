5321e63d7490f944d38aeab7c71ea1bf2cb95365
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2026-03-24 00:14:25 -0500

    Updated OBB3D.cs

--- Utilities/3D/OBB3D.cs
+++ Utilities/3D/OBB3D.cs
@@ -7,20 +7,16 @@ namespace Sin3D._OBB3D;
 /// </summary>
 public class OrientedBoundingBox3D
 {
-    Vector3[] vertices;
     /// <summary>
     /// The array of vertices of the box.
     /// </summary>
-    public Vector3[] Vertices => vertices;
+    public Vector3[] Vertices { get; }
 
     /// <summary>
     /// Creates a new oriented bounding box from an axis-aligned bounding box.
     /// </summary>
     /// <param name="AABB"></param>
-    public OrientedBoundingBox3D (BoundingBox AABB)
-    {
-        vertices = AABB.GetCorners();
-    }
+    public OrientedBoundingBox3D(BoundingBox AABB) => Vertices = AABB.GetCorners();
 
     /// <summary>
     /// Transforms the vertices of the oriented bounding box by a given transformation.
@@ -28,85 +24,84 @@ public class OrientedBoundingBox3D
     /// <param name="transform">The transformation that will be applied to the vertices.</param>
     public void TransformVertices(Matrix transform)
     {
-        for (int i = 0; i < vertices.Length; i++)
+        for (int i = 0; i < Vertices.Length; i++)
         {
-            vertices[i] = Vector3.Transform(vertices[i], transform);
+            Vertices[i] = Vector3.Transform(Vertices[i], transform);
         }
     }
 
     /// <summary>
     /// Checks if 2 oriented bounding boxes intersect.
     /// </summary>
-    /// <param name="box2">The other box.</param>
+    /// <param name="otherBox">The other box.</param>
     /// <returns>Boolean - whether or not an intersection was detected.</returns>
-    public bool Intersects(OrientedBoundingBox3D box2)
+    public bool Intersects(OrientedBoundingBox3D otherBox)
     {
-        Vector3[] box1LocalAxes = GetLocalAxes(vertices);
-        Vector3[] box2LocalAxes = GetLocalAxes(box2.Vertices);
-        Vector3[] crossAxes = GetCrossAxes(box1LocalAxes, box2LocalAxes);
-        
+        var box1LocalAxes = GetLocalAxes(Vertices);
+        var box2LocalAxes = GetLocalAxes(otherBox.Vertices);
+        var crossAxes = GetCrossAxes(box1LocalAxes, box2LocalAxes);
+
         //merging the three axis arrays, normalizing the axes, and removing any axes with a length that tends to zero
-        List<Vector3> normalizedAxes = MergeAndNormalizeAxisArrays(box1LocalAxes, box2LocalAxes, crossAxes);
+        var normalizedAxes = MergeAndNormalizeAxisArrays(box1LocalAxes, box2LocalAxes, crossAxes);
 
         //running the collision checks using SAT (separating axis theorem)
         foreach (Vector3 axis in normalizedAxes)
         {
             //projecting boxes
-            float[] projectedBoxA = GetProjectedPoints(vertices, axis);
+            float[] projectedBoxA = GetProjectedPoints(Vertices, axis);
             float projectedMinA = projectedBoxA.Min();
             float projectedMaxA = projectedBoxA.Max();
 
-            float[] projectedBoxB = GetProjectedPoints(box2.Vertices, axis);
+            float[] projectedBoxB = GetProjectedPoints(otherBox.Vertices, axis);
             float projectedMinB = projectedBoxB.Min();
             float projectedMaxB = projectedBoxB.Max();
 
             //checking if there is no overlap between the two boxes projected onto the axis
-            if (
-                (projectedMaxA < projectedMinB) ||
-                (projectedMaxB < projectedMinA)
-            )
+            if (projectedMaxA < projectedMinB || projectedMaxB < projectedMinA)
             {
                 //returning false if the ranges do not overlap (there is an axis on which the boxes do not intersect)
                 return false;
             }
         }
+
         return true;
     }
 
     //method for getting the local axes from a series of vertices from a list of vertices from an OBB
-    Vector3[] GetLocalAxes(Vector3[] verts)
+    private Vector3[] GetLocalAxes(Vector3[] verts)
     {
         Vector3[] localAxes =
         [
-            verts[0] - verts[1],    //local x
-            verts[0] - verts[3],    //local y
-            verts[0] - verts[4]     //local z
+            verts[0] - verts[1], //local x
+            verts[0] - verts[3], //local y
+            verts[0] - verts[4] //local z
         ];
         return localAxes;
     }
 
     //method for getting the cross axes of the local box1 & box2 axes
-    Vector3[] GetCrossAxes(Vector3[] box1LocalAxes, Vector3[] box2LocalAxes)
+    private Vector3[] GetCrossAxes(Vector3[] box1LocalAxes, Vector3[] box2LocalAxes)
     {
-        Vector3[] crossAxes = new Vector3[9];
+        var crossAxes = new Vector3[9];
         for (int i = 0; i < box1LocalAxes.Length; i++)
         {
             for (int j = 0; j < box2LocalAxes.Length; j++)
             {
                 Vector3 crossAxis = Vector3.Cross(box1LocalAxes[i], box2LocalAxes[j]);
-                crossAxes[(i * 3) + j] = crossAxis;
+                crossAxes[i * 3 + j] = crossAxis;
             }
         }
+
         return crossAxes;
     }
 
     //method for getting a list of normalized axes (excluding any that tend to zero)
-    List<Vector3> MergeAndNormalizeAxisArrays(Vector3[] arr1, Vector3[] arr2, Vector3[] arr3)
+    private List<Vector3> MergeAndNormalizeAxisArrays(Vector3[] arr1, Vector3[] arr2, Vector3[] arr3)
     {
-        List<Vector3> normalizedAxes = new ();
+        List<Vector3> normalizedAxes = [];
 
         //iterating over all axes
-        Vector3[] axes = new Vector3[arr1.Length + arr2.Length + arr3.Length];
+        var axes = new Vector3[arr1.Length + arr2.Length + arr3.Length];
         arr1.CopyTo(axes, 0);
         arr2.CopyTo(axes, arr1.Length);
         arr3.CopyTo(axes, arr1.Length + arr2.Length);
@@ -117,11 +112,12 @@ public class OrientedBoundingBox3D
                 normalizedAxes.Add(Vector3.Normalize(axis));
             }
         }
+
         return normalizedAxes;
     }
 
     //method for getting the projected points of a given series of vertices projected onto a given axis
-    float[] GetProjectedPoints(Vector3[] verts, Vector3 axis)
+    private float[] GetProjectedPoints(Vector3[] verts, Vector3 axis)
     {
         float[] projectedPoints = new float[verts.Length];
         for (int i = 0; i < verts.Length; i++)
@@ -129,6 +125,7 @@ public class OrientedBoundingBox3D
             float projectedPoint = Vector3.Dot(verts[i], axis);
             projectedPoints[i] = projectedPoint;
         }
+
         return projectedPoints;
     }
 }
